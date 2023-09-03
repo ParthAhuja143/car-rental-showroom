8ce193fb33d841ca0e606e455af823e4f74ab5b2
Parth Ahuja <parth.a@appdeskservices.com>
2023-09-03 20:49:31 +0530

    App changes

---
+++ src/components/index.ts
@@ -0,0 +1,23 @@
+import Hero from './Hero';
+import CustomButton from './CustomButton';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import ShowMore from './ShowMore';
+import CarCard from './CareCard';
+import CarDetails from './CarDetails';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+import SearchBar from './Searchbar';
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    ShowMore,
+    CarCard,
+    CarDetails,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer
+}
